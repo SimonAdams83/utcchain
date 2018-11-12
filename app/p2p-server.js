5f7bc72c58e0aecab33ff1c431ae950e14cadff7
SimonAdams83 <44026708+SimonAdams83@users.noreply.github.com>
2018-11-12 15:41:50 +0000

    Update p2p-server.js

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -1,5 +1,5 @@
 const Websocket = require('ws');
-const P2P_PORT = process.env.P2P_PORT || 5001;
+const P2P_PORT = process.env.P2P_PORT || 167.99.89.10:5001;
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 const MESSAGE_TYPES = {
   chain: 'CHAIN',
